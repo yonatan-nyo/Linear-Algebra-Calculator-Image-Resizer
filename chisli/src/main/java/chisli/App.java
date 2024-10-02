274ee9a4e0b523a11351f41d7a6144ad09fec053
yonatan-nyo <yonatan.njoto@gmail.com>
2024-10-02 11:17:36 +0700

    added css configuration

--- chisli/src/main/java/chisli/App.java
+++ chisli/src/main/java/chisli/App.java
@@ -18,17 +18,26 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        loadCSS("primary");
         stage.setScene(scene);
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+        loadCSS(fxml);
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static void loadCSS(String fxml) {
+        String cssFile = fxml + ".css";
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add(App.class.getResource("global.css").toExternalForm());
+        scene.getStylesheets().add(App.class.getResource(cssFile).toExternalForm());
     }
 
     public static void main(String[] args) {
